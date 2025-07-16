887b70da791d715fd5e577e3af01ef1d50f11773
Ramana Anjani <ramananjani@Ramanas-MacBook-Pro.local>
2025-07-17 02:00:09 +0530

    Updated the App.js file to the previous

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import { ThemeProvider } from '@mui/material/styles';
+import customTheme from './theme/customTheme';
+import AppRoutes from './AppRoutes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={customTheme}>
+			<Router> {/* Wrap your app with Router */}
+				<div className="App">
+					<div className="navbar-container">
+						<Navbar />
+					</div>
+					<AppRoutes />
+					<Footer />
+				</div>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
